34c69680d3943d92627edf79c7d3bc2579431106
Deep Prajapati <dvprajapati7212@gmail.com>
2026-01-16 10:00:52 +0530

    feat:implemented get all approved places controller

--- server/src/controllers/place.controller.js
+++ server/src/controllers/place.controller.js
@@ -16,4 +16,26 @@ export const createPlace = asyncHandler(async (req, res) => {
         throw new ApiError(400,"Failed to create place")
     }
     return res.status(201).json(new ApiResponse(201,place,"Place submitted for approval"))
+});
+
+
+// Get All Approved Places (PUBLIC)
+
+export const getPlaces = asyncHandler(async (req, res) => {
+    const { category, location, page = 1, limit = 10 } = req.query;
+
+    const query = { status: "approved" };
+
+    if (category) {
+        query.category = category;
+    }
+
+    if (location) {
+        query["location.city"] = location;
+    }
+
+    const places = await Place.find(query).skip((Number(page) - 1) * Number(limit)).limit(Number(limit)).sort({ isFeatured: -1, createdAt: -1 });
+    const total = await Place.countDocuments(query);
+
+    return res.status(200).json(new ApiResponse(200, places, "Places fetched successfully"));
 })
