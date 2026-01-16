252d8b8148dd151c071283a45dda74197fa9d504
Deep Prajapati <dvprajapati7212@gmail.com>
2026-01-16 10:13:39 +0530

    feat: added get specific place by id and update place by owner logic

--- server/src/controllers/place.controller.js
+++ server/src/controllers/place.controller.js
@@ -35,7 +35,49 @@ export const getPlaces = asyncHandler(async (req, res) => {
     }
 
     const places = await Place.find(query).skip((Number(page) - 1) * Number(limit)).limit(Number(limit)).sort({ isFeatured: -1, createdAt: -1 });
+
+    if (!places) {
+        throw new ApiError(404,"No places found")
+    }
     const total = await Place.countDocuments(query);
 
     return res.status(200).json(new ApiResponse(200, places, "Places fetched successfully"));
-})
+});
+
+// Get Place by ID(PUBLIC)
+
+export const getPlaceById = asyncHandler(async (req, res) => {
+    const place = await Place.findOne({
+        _id: req.params.id,
+        status: "approved"
+    }).populate("owner", "name");
+
+    if (!place) {
+        throw new ApiError(404, "Place not found");
+    }
+
+    place.views += 1;
+    await place.save();
+
+    return res.status(200).json(new ApiResponse(200, place, "Place fetched successfully"));
+});
+
+// Update Place (OWNER)
+
+export const updatePlace = asyncHandler(async (req, res) => {
+    const place = await Place.findById(req.params.id);
+
+    if (!place) {
+        throw new ApiError(404, "Place not found");
+    }
+
+    if (place.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403,"You are not authorized to update this place")
+    }
+
+    Object.assign(place, req.body);
+    place.status = "pending"; //Re-approval required after update
+    await place.save();
+
+    return res.status(200).json(new ApiResponse(200, place, "Place updated and sent for re-approval"));
+});
