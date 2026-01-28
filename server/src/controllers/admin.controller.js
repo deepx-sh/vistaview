e57ea842b7cf6d4a3fb76778860bb4e652d79f07
Deep Prajapati <dvprajapati7212@gmail.com>
2026-01-28 13:02:57 +0530

    feat: implement reviewOwner function for processing owner status updates

--- server/src/controllers/admin.controller.js
+++ server/src/controllers/admin.controller.js
@@ -13,4 +13,34 @@ export const getPendingOwners = asyncHandler(async (req, res) => {
         throw new ApiError(404, "No pending owners found");
     }
     return res.status(200).json(new ApiResponse(200,owners,"Pending owners retrieved successfully"))
+});
+
+
+export const reviewOwner = asyncHandler(async (req, res) => {
+    const { status, rejectedReason } = req.body;
+
+    const user = await User.findById(req.params.userId);
+
+    if (!user || !user.ownerProfile?.status !== "pending") {
+        throw new ApiError(404, "Owner not in pending status");
+    }
+
+    if (status === "approved") {
+        user.ownerProfile.status = "approved";
+        user.ownerProfile.approvedAt = new Date();
+        user.role="owner"
+    }
+
+    if (status === "rejected") {
+        if (!rejectedReason) {
+            throw new ApiError(400,"Rejected reason is required")
+        } else {
+            user.ownerProfile.status = "rejected";
+            user.ownerProfile.rejectedReason = rejectedReason;
+        }
+    }
+
+    await user.save();
+
+    return res.status(200).json(new ApiResponse(200,{},`Owner request ${status}`))
 })
