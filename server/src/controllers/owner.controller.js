a0e40b30769ec7bd9899391bc04352ff79c0354e
Deep Prajapati <dvprajapati7212@gmail.com>
2026-01-29 10:34:42 +0530

    feat: implement replyToReview function for handling owner replies to reviews and notifications

--- server/src/controllers/owner.controller.js
+++ server/src/controllers/owner.controller.js
@@ -1,7 +1,10 @@
 import cloudinary from '../config/cloudinary.js';
+import { Place } from '../models/place.model.js';
+import { Review } from '../models/review.model.js';
 import { ApiError } from '../utils/ApiError.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import {asyncHandler} from "../utils/asyncHandler.js"
+import createNotification from '../utils/createNotification.js';
 
 export const applyForOwner = asyncHandler(async (req, res) => {
     const user = req.user;
@@ -37,4 +40,49 @@ export const applyForOwner = asyncHandler(async (req, res) => {
 
     await user.save();
     return res.status(201).json(new ApiResponse(200,{},"Owner verification request submitted"))
+});
+
+export const replyToReview = asyncHandler(async (req, res) => {
+    const { reviewId } = req.params;
+    const { text } = req.body;
+
+    const review = await Review.findById(reviewId);
+
+    if (!review || review.isDeleted) {
+        throw new ApiError(404,"Review not found")
+    }
+
+    const place = await Place.findById(review.place);
+    if (!place) {
+        throw new ApiError(404,"Place not found")
+    }
+
+    // Ownership check
+    if (place.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403,"Not authorized to reply to this review")
+    }
+
+    if (review.ownerReply?.text) {
+        throw new ApiError(400,"Reply already exists for this review")
+    }
+
+    review.ownerReply = {
+        text,
+        repliedAt:new Date()
+    }
+
+    await review.save();
+
+    // Notify user 
+    await createNotification({
+        user: review.user,
+        type: "reply",
+        message: `Owner replied to your review on ${place.name}`,
+        link:`place/${place._id}`
+    })
+
+    const responseData = {
+        reply:review.ownerReply
+    }
+    return res.status(200).json(new ApiResponse(200,responseData,"Reply added successfully"))
 })
