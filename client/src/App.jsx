f151894f72f87fd4579f5877eb1ba2c432f78384
Deep Prajapati <dvprajapati7212@gmail.com>
2026-02-12 21:32:08 +0530

    init: setup frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='min-h-screen flex justify-center items-center'>
+        <button className='bg-primary px-4 py-2  rounded-sm hover:bg-primary-hover cursor-pointer transition duration-500'>Welcome to VistaView</button>
+      </div>
+    </>
+  )
+}
+
+export default App
