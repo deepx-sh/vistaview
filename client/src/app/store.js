4ea6b0a65a51ec71220da623bf12abff3f7239df
Deep Prajapati <dvprajapati7212@gmail.com>
2026-02-17 08:59:12 +0530

    feat:add middleware in store to cache data

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from '../features/auth/authSlice.js';
+import { baseApi } from "../services/api/baseApi.js";
 
 export const store = configureStore({
     reducer: {
-        auth:authReducer,
-    }
+        auth: authReducer,
+        [baseApi.reducerPath]:baseApi.reducer
+    },
+    middleware:(getDefaultMiddleware)=> getDefaultMiddleware().concat(baseApi.middleware)
 })
