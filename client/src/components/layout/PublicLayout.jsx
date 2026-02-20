37cda40abf31fe21dd5baac9ebf38d233ddd67bb
Deep Prajapati <dvprajapati7212@gmail.com>
2026-02-20 21:55:09 +0530

    feat: add public layout which shows navbar main content and footer

---
+++ client/src/components/layout/PublicLayout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import React from 'react'
+import Footer from "./Footer";
+
+const PublicLayout = () => {
+  return (
+      <div className="min-h-screen flex flex-col bg-background">
+          <Navbar />
+          
+          <main className="flex-1">
+              <Outlet/>
+          </main>
+          <Footer/>
+    </div>
+  )
+}
+
+export default PublicLayout
