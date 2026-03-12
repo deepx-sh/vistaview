39505de8abe30a5397a0fdb0d61e22376d00ee86
Deep Prajapati <dvprajapati7212@gmail.com>
2026-03-12 22:08:59 +0530

    fix: owner login route

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
             if (res.data.user.role === "admin") {
                 navigate("/admin/dashboard")
             } else if (res.data.user.role === "owner") {
-                navigate("/owner/dashboard")
+                navigate("/owner")
             } else {
                 navigate("/")
             }
