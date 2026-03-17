ac29d9eb484caa38f2590c1e31e98dafa5891e12
Deep Prajapati <dvprajapati7212@gmail.com>
2026-03-17 13:02:58 +0530

    feat: add get owner reviews route in backend and install package brevo

--- server/src/routes/owner.routes.js
+++ server/src/routes/owner.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
 import validate from '../middlewares/validate.middleware.js';
-import { applyForOwner, deleteReply, replyToReview, updateReply } from '../controllers/owner.controller.js';
+import { applyForOwner, deleteReply, getOwnerReviews, replyToReview, updateReply } from '../controllers/owner.controller.js';
 import { applyOwnerSchema } from '../validators/owner.validators.js';
 import uploadOwnerDocs from '../config/multerOwnerDocs.js';
 import { authorizeRoles } from '../middlewares/role.middleware.js';
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.get("/dashboard",authMiddleware,authorizeRoles("owner"),getOwnerDashBoard)
 router.post("/apply", authMiddleware, uploadOwnerDocs.array("documents", 3), validate(applyOwnerSchema), applyForOwner);
-
+router.get("/reviews",authMiddleware,authorizeRoles("owner"),getOwnerReviews)
 router.post("/reviews/:reviewId/reply", authMiddleware, authorizeRoles("owner"), validate(replySchema), replyToReview);
 
 router.put("/reviews/:reviewId/reply",authMiddleware,authorizeRoles("owner"),validate(replySchema),updateReply)
