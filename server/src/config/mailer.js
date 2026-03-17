e5796a00b46726f670a09fe62f6ebbdef31e3816
Deep Prajapati <dvprajapati7212@gmail.com>
2026-03-17 13:02:00 +0530

    feat: implemented brevo api to send email

--- server/src/config/mailer.js
+++ server/src/config/mailer.js
@@ -1,28 +1,32 @@
-import sgMail from '@sendgrid/mail';
+import { BrevoClient } from "@getbrevo/brevo";
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const client = new BrevoClient({
+    apiKey:process.env.BREVO_API_KEY
+});
 
 const sendEmail = async ({ to, subject, html }) => {
     try {
-        const mailOptions = {
-            from: `VistaView  <${process.env.SENDGRID_FROM_EMAIL}>`,
-            to,
-            subject,
-            html
-        }
-
-        await sgMail.send(mailOptions);
+        await client.transactionalEmails.sendTransacEmail({
+            subject: subject,
+            htmlContent: html,
+            sender: {
+                name: "VistaView",
+                email:process.env.BREVO_FROM_EMAIL
+            },
+            to: [
+                {
+                    email:to
+                }
+            ]
+        })
         console.log(`Successfully email sent to ${to}`);
+        
     } catch (error) {
         console.error("Failed to send email");
-
-        if (error.response) {
-            console.error(error.response.body);
-        } else {
-            console.error(error.message);
-        }
+        console.error(error.message);
         throw new Error("Email could not be sent")
     }
 };
 
-export default sendEmail;
+
+export default sendEmail
