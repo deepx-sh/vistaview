2e4b34eb78cdd8035dd38bcf13d3b3a94f593973
Deep Prajapati <dvprajapati7212@gmail.com>
2026-04-03 22:17:18 +0530

    fix: add sidebar in admin dashboard for mobile view

--- client/src/components/layout/AdminLayout.jsx
+++ client/src/components/layout/AdminLayout.jsx
@@ -1,16 +1,30 @@
 import { Outlet } from "react-router-dom";
 import AdminSidebar from "../admin/AdminSidebar";
-
+import { useState } from "react";
+import { Menu } from "lucide-react";
 import React from 'react'
 
 const AdminLayout = () => {
+  const [sidebarOpen,setSidebarOpen]=useState(false)
   return (
-      <div className="flex min-h-screen">
-          <AdminSidebar />
-          <main className="flex-1 p-6 bg-background">
-              <Outlet/>
-          </main>
-    </div>
+    <div className="flex h-screen overflow-hidden bg-background">
+      <AdminSidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
+
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <header className="lg:hidden flex items-center justify-between h-16 px-4 bg-surface border-b border-border z-30">
+          <button onClick={() => setSidebarOpen(true)} className="text-text-secondary p-2 -ml-2 hover:bg-gray-100 rounded-md transition-colors">
+            <Menu size={24}/>
+          </button>
+
+          <span className="font-semibold text-text-primary">Admin Dashboard</span>
+          <div className="w-8"></div>
+        </header>
+
+        <main className="flex-1 overflow-x-hidden overflow-y-auto p-4 sm:p-6 lg:p-8">
+          <Outlet/>
+        </main>
+      </div>
+     </div>
   )
 }
 
